6716e979fc3065fb79485b52d528c75c2bcdb95d
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-04-12 17:06:34 +0200

    Removed comment

--- Event-driven Asynchronous Programming/ast-analyzer/src/main/java/pcd/assignment02/TestJavaParser.java
+++ Event-driven Asynchronous Programming/ast-analyzer/src/main/java/pcd/assignment02/TestJavaParser.java
@@ -47,14 +47,11 @@ public class TestJavaParser {
 
 	public static void main(String[] args) throws Exception {
 		CompilationUnit cu = StaticJavaParser.parse(new File("src/pcd/ass02/TestJavaParser.java"));
-		/*
 		var methodNames = new ArrayList<String>();
 		var methodNameCollector = new MethodNameCollector();
 		methodNameCollector.visit(cu,methodNames);
 		methodNames.forEach(n -> System.out.println("MethodNameCollected:" + n));
-		 */
 		var fullc = new FullCollector();
 		fullc.visit(cu, null);
-		
 	}
 }
